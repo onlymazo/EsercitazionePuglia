e75e228d8e05f5bf7547add976d00030b553920e
simon <simon@192.168.1.7>
2022-06-28 17:31:11 +0200

    eliminato com.simone.test aggiunto create_script.sql

--- GestioneCorsi/src/main/java/com/simone/test/Test.java
+++
@@ -1,9 +0,0 @@
-package com.simone.test;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("TEST");
-		System.out.println("Ciao simo");
-		System.out.println("quell'insana voglia di crepare male");
-	}
-}
